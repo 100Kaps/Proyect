b9eaa57d78554a80dca0724d9d7f869003e1628c
kenny <kenny@Kenny>
2024-03-04 23:59:25 -0600

    Guardado de proyect

---
+++ src/main/java/com/mycompany/test/Menu.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.test;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author kenny
+ */
+public class Menu {
+    public static void main(String[] args){
+     Scanner scanner =new Scanner(System.in);
+     System.out.println("Bienvenido, por favor ingrese el numero de la opcion que desee operar");
+     System.out.println("Objetos y Recursividad");
+     System.out.println("Fase 2");
+     System.out.println("Fase 3");
+     System.out.println("Fase 4");
+    }
+}
---
+++ src/main/java/com/mycompany/test/Test.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.test;
+
+/**
+ *
+ * @author kenny
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Ludwin");
+    }
+}
